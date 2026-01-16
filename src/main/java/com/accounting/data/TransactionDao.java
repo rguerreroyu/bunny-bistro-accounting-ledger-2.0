53cc774dc122e987ac94516fab4147a68d084ad3
Jasmine <jasminetntu@gmail.com>
2026-01-16 12:37:49 -0800

    add the get methods (#4)

--- src/main/java/com/accounting/data/TransactionDao.java
+++ src/main/java/com/accounting/data/TransactionDao.java
@@ -2,7 +2,12 @@ package com.accounting.data;
 
 import com.accounting.models.Transaction;
 
+import java.util.List;
+
 public interface TransactionDao {
+    public List<Transaction> getAllTransactions();
+    public List<Transaction> getAllDeposits();
+    public List<Transaction> getAllPayments();
     public void update(int transactionId, Transaction transaction);
     public void delete(int transactionId);
 
--- src/main/java/com/accounting/data/mysql/MySqlTransactionDao.java
+++ src/main/java/com/accounting/data/mysql/MySqlTransactionDao.java
@@ -8,12 +8,87 @@ import javax.sql.DataSource;
 import java.sql.*;
 import java.time.LocalDateTime;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class MySqlTransactionDao extends MySqlDaoBase implements TransactionDao {
 
     public MySqlTransactionDao(DataSource dataSource)
     {
         super(dataSource);
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        {
+            String sql = "SELECT * FROM bunnybistroledger.transactions";
+            List<Transaction> transactions = new ArrayList<>();
+
+            try (Connection connection = getConnection()) {
+                PreparedStatement statement = connection.prepareStatement(sql);
+
+
+                ResultSet row = statement.executeQuery();
+
+
+                while (row.next()) {
+                    transactions.add(mapRow(row));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            return transactions;
+        }
+    }
+
+    @Override
+    public List<Transaction> getAllDeposits() {
+        {
+            String sql = "SELECT * FROM bunnybistroledger.transactions WHERE is_deposit = TRUE";
+            List<Transaction> transactions = new ArrayList<>();
+
+            try (Connection connection = getConnection()) {
+                PreparedStatement statement = connection.prepareStatement(sql);
+
+
+                ResultSet row = statement.executeQuery();
+
+
+                while (row.next()) {
+                    transactions.add(mapRow(row));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            return transactions;
+        }
+    }
+
+    @Override
+    public List<Transaction> getAllPayments() {
+        {
+            String sql = "SELECT * FROM bunnybistroledger.transactions WHERE is_deposit = FALSE";
+            List<Transaction> transactions = new ArrayList<>();
+
+            try (Connection connection = getConnection()) {
+                PreparedStatement statement = connection.prepareStatement(sql);
+
+
+                ResultSet row = statement.executeQuery();
+
+
+                while (row.next()) {
+                    transactions.add(mapRow(row));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            return transactions;
+        }
     }
 
     @Override
