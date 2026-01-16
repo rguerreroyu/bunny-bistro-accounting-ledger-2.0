03015f234b2d7a551d74454abac257341d138ca7
Jasmine <jasminetntu@gmail.com>
2026-01-16 12:08:52 -0800

    implement user stuff (#4)

--- src/main/java/com/accounting/data/UserDao.java
+++ src/main/java/com/accounting/data/UserDao.java
@@ -1,4 +1,14 @@
 package com.accounting.data;
 
+import com.accounting.models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> getAll();
+    User getUserById(int userId);
+    User getByUserName(String username);
+    int getIdByUsername(String username);
+    User create(User user);
+    boolean exists(String username);
 }
